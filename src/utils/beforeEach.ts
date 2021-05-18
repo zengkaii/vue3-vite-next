f146c471badfc65807c988b3f47290126751bd3f
zengkai <1965599425@qq.com>
2021-05-18 17:34:47 +0800

    fix

--- src/utils/beforeEach.ts
+++ src/utils/beforeEach.ts
@@ -1,7 +1,7 @@
 
 import router from '@/router/index'
 import localStorage from '@/utils/localStorage'
-const a = 123
+
 router.beforeEach((to, from, next) => {
   let token = localStorage.get('vite-token')
   if (token) {
